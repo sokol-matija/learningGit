853068e89141d4a7134f6a7eadd0c06fdbd9e449
zerocool247 <sokol.matija@gmail.com>
2020-02-08 20:58:56 +0100

    Initial commit

---
+++ hederfille.h
@@ -0,0 +1,17 @@
+class hederfille
+{
+private:
+    int broj;
+public:
+    hederfille(int broj);
+    ~hederfille();
+};
+
+hederfille::hederfille(int broj)
+{
+}
+
+hederfille::~hederfille()
+{
+}
+    
---
+++ source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+for (int ti = 0; ti < 23; ti++)
+{   
+    cout << "ucim koristiti git modificirao sma " << endl;
+    
+}
+
+    return 0;
+}
